91442e3a3df9984df31333eb48da9afa4098d83d
Brodaigh Lynch <brodaigh@Mac.(none)>
2008-09-21 18:50:04 +1000

    easier on the eyes

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,7 +1,10 @@
 module VideosHelper
   
   def embed(video)
-    "<object width=\"425\" height=\"355\"><param name=\"movie\" value=\"http://www.youtube.com/v/#{video}&hl=en\"></param><param name=\"wmode\" value=\"transparent\"></param><embed src=\"http://www.youtube.com/v/#{video}&hl=en\" type=\"application/x-shockwave-flash\" wmode=\"transparent\" width=\"425\" height=\"355\"></embed></object>"
+    "<object width=\"100%\" height=\"100%\"><param name=\"movie\" 
+    value=\"http://www.youtube.com/v/#{video}&hl=en\"></param><param name=\"wmode\" 
+    value=\"transparent\"></param><embed src=\"http://www.youtube.com/v/#{video}&hl=en\" t
+    ype=\"application/x-shockwave-flash\" wmode=\"transparent\" width=\"50%\" height=\"500px\"></embed></object>"
   end
   
 end
